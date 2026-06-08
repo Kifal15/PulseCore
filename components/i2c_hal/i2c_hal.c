ef4bcbd27de1131cde10e6583eab270c621f8df3
Kifal15 <kifal531@gmail.com>
2026-06-08 12:39:19 +0800

    Implement I2C HAL initialization and register write support

--- components/i2c_hal/i2c_hal.c
+++ components/i2c_hal/i2c_hal.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "i2c_hal.h"
 #include "driver/i2c.h"
-
+#include "freertos/FreeRTOS.h"
 #define I2C_SCL_IO 22 
 #define I2C_SDA_IO 21 
 #define I2C_MASTER_FREQ_HZ 100000
@@ -23,10 +23,64 @@ i2c_config_t conf = {
 
 esp_err_t err = i2c_param_config(I2C_NUM_0 , &conf);
 
+if (err != ESP_OK)
+    {
+        return false ;
+    }   
 
 
+err = i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER , 0 ,0,0 );
+
+if(err != ESP_OK)
+    {
+        return false ;
+    }
 
 
+return true ;
 
 
 }
+
+
+
+bool i2c_hal_write_reg( uint8_t device_addr , uint8_t register_addr , uint8_t value )
+    {
+
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        if(cmd == NULL ){
+
+            return false ; 
+
+        }
+
+        i2c_master_start(cmd);
+
+        i2c_master_write_byte(cmd, (device_addr << 1 ) | I2C_MASTER_WRITE , true);
+
+        i2c_master_write_byte(cmd , register_addr , true);
+
+        i2c_master_write_byte(cmd , value , true) ;
+        
+        i2c_master_stop(cmd);
+
+       esp_err_t err = i2c_master_cmd_begin(
+        I2C_NUM_0 , 
+        cmd , 
+        pdMS_TO_TICKS(1000)
+       );
+        i2c_cmd_link_delete(cmd);
+
+       if (err != ESP_OK){
+
+        return false ; 
+
+           }
+        
+
+        
+        return true ;
+
+        
+
+    }   
